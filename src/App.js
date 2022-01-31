86f671843d73383badabb572242e9a614de89cfc
Your Name <you@example.com>
2022-01-31 15:11:44 +0200

    UseStateInitilize

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import ColorPicker from './ColorPicker';
+import ColorPickerInit from './ColorPickerInit';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
        <h1>React-Hook</h1>
+       <h3>Use State</h3>
+       <ColorPicker />
+       <h3>Initialize Use State</h3>
+       <ColorPickerInit />
       </header>
-    </div>
+   </div>
+    
   );
 }
 
